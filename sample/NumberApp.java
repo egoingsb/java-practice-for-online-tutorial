e310d512854687e198b45934700e5d34e5f4ff61
egoing <egoing@gmail.com>
2019-08-24 16:26:34 +0900

    Number

---
+++ sample/NumberApp.java
@@ -0,0 +1,16 @@
+/*
+Goal : 
+    1. Understanding Number data type.
+Step
+    javac --class-path "." NumberApp.java; java --class-path "." NumberApp
+*/
+public class NumberApp{
+    public static void main(String[] args){
+        System.out.println(8805); // integer number
+        System.out.println(88.05); // real number
+        System.out.println(4+2); // 6
+        System.out.println(4-2); // 2
+        System.out.println(4*2); // 8
+        System.out.println(4/2); // 2
+    }
+}
